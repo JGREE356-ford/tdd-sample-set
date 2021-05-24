f6939b2936009bdc05e917a46242cbbb6124f491
JGREE356 <JGREE356@ford.com>
2021-05-24 16:06:55 -0400

    Skeleton Set Class

--- app/src/main/java/com/ford/tddsampleset/UnOrderedSet.kt
+++ app/src/main/java/com/ford/tddsampleset/UnOrderedSet.kt
@@ -1,4 +1,20 @@
 package com.ford.tddsampleset
 
-class UnOrderedSet {
+class UnOrderedSet<T> {
+
+    init {}
+
+    fun add(obj: T) { }
+
+    fun insert(obj: T, index: Int) {}
+
+    fun contains(obj: T) {}
+
+    fun getItem(index: Int): T { return Object() as T }
+
+    fun remove(obj: T) {}
+
+    fun getAll(): List<T> { return emptyList() }
+
+    fun size(): Int { return 0 }
 }
---
+++ app/src/test/java/com/ford/tddsampleset/UnOrderedSetTest.kt
@@ -0,0 +1,15 @@
+package com.ford.tddsampleset
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UnOrderedSetTest  {
+
+    @Test
+    fun add_itemGiven_addsItemToSet() {
+
+        // Get started here
+
+        assertEquals(true, true)
+    }
+}
