96e9e4c7d379310309462783a5e7ac33f9b533f9
JGREE356 <JGREE356@ford.com>
2021-08-12 12:27:18 -0400

    Convert to pure IntellJ project

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.ford.tddsampleset {
+    requires kotlin.stdlib;
+    requires junit;
+}
---
+++ demo/src/test/kotlin/com/ford/tddsampleset/SetKotlinTest.kt
@@ -0,0 +1,36 @@
+package com.ford.tddsampleset
+
+/* TODO
+
+ isEmpty
+
+ add
+
+ getSize
+
+ contains
+
+ remove
+
+ clear
+
+ */
+
+// Implement set using an Array of Strings of size 5
+// add minimal code to make the tests pass
+
+// Test case naming convention
+// <action/functionName>_<Conditions>_<result>
+
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+class SetKotlinTest  {
+
+    @Test
+    fun add_itemGiven_addsItemToSet() {
+        // Start here
+
+        assertEquals(true, true)
+    }
+}
